9f41446ad180934102cf802d5d4e0f2d46fbe185
takedasan <takedasan593@gmail.com>
2018-10-25 01:01:24 +0900

    mod login command

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-$(aws ecr get-login --no-include-email --region us-east-1)
+(aws ecr get-login --no-include-email --region us-east-1)
 docker build -t sample-repository .
 docker tag sample-repository:latest 273930194378.dkr.ecr.us-east-1.amazonaws.com/sample-repository:latest
 docker push 273930194378.dkr.ecr.us-east-1.amazonaws.com/sample-repository:latest
