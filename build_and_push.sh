513dd5a6f2f7f62f0467d2788694825d9d7d9477
takedasan <takedasan593@gmail.com>
2018-10-25 01:06:48 +0900

    mod shell

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-(aws ecr get-login --no-include-email --region us-east-1)
+apt-get update && apt-get install -y awscli
+
+$(aws ecr get-login --no-include-email --region us-east-1)
 docker build -t sample-repository .
 docker tag sample-repository:latest 273930194378.dkr.ecr.us-east-1.amazonaws.com/sample-repository:latest
 docker push 273930194378.dkr.ecr.us-east-1.amazonaws.com/sample-repository:latest
